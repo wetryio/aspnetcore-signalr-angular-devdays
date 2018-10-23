bf48d9e7dd27ca475e00aba1a8b8e053ce5aab95
Laura Ait braham <Laura@Lauras-Air.mshome.net>
2018-10-23 12:55:00 +0200

    Init repository

---
+++ src/server/Controllers/AccountController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+    }
+}
