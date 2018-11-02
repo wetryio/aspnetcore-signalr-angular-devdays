db8c602aa78e312a18f210715bee0adf9a9b46da
Patrick Grasseels <grasseelsp@hotmail.com>
2018-11-02 12:54:39 +0100

    Add controller and Hub

--- src/server/Startup.cs
+++ src/server/Startup.cs
@@ -28,6 +28,8 @@ namespace Server
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMemoryCache();
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddSignalR();
