5f8bc4a32412d77d02e637e5df8d4c801d417f58
David Gilson <davgilson@live.fr>
2018-11-23 09:09:15 +0100

    add auto scroll

--- src/front/src/chat/components/message-box/message-box.component.ts
+++ src/front/src/chat/components/message-box/message-box.component.ts
@@ -1,4 +1,7 @@
-import { Component, ChangeDetectionStrategy, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, ChangeDetectionStrategy, Input, OnChanges,
+  SimpleChanges, ViewChild, ElementRef, OnDestroy } from '@angular/core';
+
+import { reaction, IReactionDisposer } from 'mobx';
 
 import { User } from '../../models';
 import { MessageStore } from '../../stores';
@@ -9,17 +12,26 @@ import { MessageStore } from '../../stores';
   styleUrls: ['./message-box.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class MessageBoxComponent implements OnChanges {
+export class MessageBoxComponent implements OnChanges, OnDestroy {
 
   @Input() user: User;
+  @ViewChild('history') history: ElementRef;
 
   public messageToSend: string;
+  private messageReactionDisposer: IReactionDisposer;
 
   constructor(private messageStore: MessageStore) { }
 
   ngOnChanges(changes: SimpleChanges) {
     if (changes.user && this.user) {
       this.messageStore.switchConversation(this.user);
+      this.initScroll();
+    }
+  }
+
+  ngOnDestroy() {
+    if (this.messageReactionDisposer) {
+      this.messageReactionDisposer();
     }
   }
 
@@ -32,6 +44,24 @@ export class MessageBoxComponent implements OnChanges {
       });
     }
     this.messageToSend = '';
+  }
+
+  private initScroll() {
+    if (!this.messageReactionDisposer) {
+      this.messageReactionDisposer = reaction(() => this.messageStore.currentChat.messages, () => {
+        console.log('messaaaaage');
+        setTimeout(() => { // whait that message is added to view
+          this.scrollBottom();
+        }, 0);
+      });
+    }
+    this.scrollBottom();
+  }
+
+  private scrollBottom() {
+    const element = this.history.nativeElement;
+    const scrollTo = element.scrollHeight;
+    element.scrollTo(0, scrollTo);
   }
 
 }
